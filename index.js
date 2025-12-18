091bb1c57c32670560d47ae830e564be9947f2fa
GodwinAkhidue <ebehiakhidue2099@gmail.com>
2025-12-18 10:22:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,11 +20,8 @@ app.use(
   cors({
     origin: function (origin, callback) {
       if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
+      const isAllowed = allowedOrigins.includes(origin);
+      callback(null, isAllowed);
     },
     credentials: true,
   })
